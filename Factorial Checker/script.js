f12b6b30f6f5ded771d5bff13390257b067d66d8
Prajwal-2023 <PRAJWAL.MCA2302203@SBUP.EDU.IN>
2025-04-14 00:56:25 +0530

    Factorial Checker Done

--- Factorial Checker/script.js
+++ Factorial Checker/script.js
@@ -4,18 +4,21 @@ const showOutPut = document.querySelector('.main h2');
 
 
 function factorialOfNum(userNum) {
-
-   
-
-
+    let result = 1;
+    for(let i = userNum ; i >= 1 ; i--){
+       result *= i;
+    }
+   return result ;
 }
         buttonElem.addEventListener("click", () => {
-            const userNum = inputElem.value;
-            const factorialNum = factorialOfNum(userNum);
-            showOutPut.innerHTML = '';
-            if(isNaN(userNum)){
-            showOutPut.innerHTML += `<h6 style="text-align:center;color:orangered;">Enter A Number</h6>`;
+            const userNum = parseInt(inputElem.value);
+            // const factorialNum = factorialOfNum(userNum).toLocaleString('en-IN',{style:"currency" , currency: "INR"});
+            const factorialNum = factorialOfNum(userNum).toLocaleString('en-IN');
+            if (userNum <= 1 || isNaN(userNum)) {
+                showOutPut.innerHTML = '';
+                showOutPut.innerHTML += `<h6 style="text-align:center;color:orangered;">Enter a Number</h6>`;
             }else{
+                showOutPut.innerHTML = '';
                 showOutPut.innerHTML += `<span>Factorial is:</span><br><h6 style="text-align:center;color:orangered;">${factorialNum}</h6>`;
             }
         })
